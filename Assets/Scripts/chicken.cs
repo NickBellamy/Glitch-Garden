b0572827bd5224dde0d5ad5213e676f468c8f197
Nick Bellamy <nick.bellamy@gmail.com>
2016-01-11 17:24:48 +0000

    Add Chicken enemy to game
    Chicken enemy moves slowly but should have high health and high damage.
    Currently se to only spawn on level 3.

---
+++ Assets/Scripts/chicken.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class chicken : MonoBehaviour {
+
+	private Animator animator;
+	private Attacker attacker;
+	
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		attacker = GetComponent<Attacker>();
+		
+	}
+	
+	public void OnTriggerEnter2D (Collider2D col) {
+		GameObject obj = col.gameObject;
+		
+		if (!obj.GetComponent<Defenders>()) {
+			return;
+		}
+		
+		attacker.Attack(obj);
+		animator.SetBool("isAttacking", true);
+		
+	}
+}
