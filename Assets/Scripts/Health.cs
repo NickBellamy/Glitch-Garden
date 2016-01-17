3da6ca95e0c598a5783cd2c38bc02ec1c60cd194
Nick Bellamy <nick.bellamy@gmail.com>
2016-01-17 12:06:11 +0000

    Add health bars to defenders

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,6 +4,13 @@ using System.Collections;
 public class Health : MonoBehaviour {
 	
 	public float health = 100f;
+	public GameObject healthBar;
+	
+	private float maxHealth;
+	
+	void Start() {
+		maxHealth = health;
+	}
 	
 	public void DealDamage(float damage) {
 		health -= damage;
@@ -13,6 +20,9 @@ public class Health : MonoBehaviour {
 			//used to keep track of the rustling sound from the hedges.
 			gameObject.transform.position += new Vector3(1000000.0f, 1000000.0f, 0.0f);
 			Invoke ("DestroyObject", 2.0f);
+		}
+		if (gameObject.GetComponent<Defenders>()) {
+			healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(health / maxHealth, 0f);
 		}
 	}
 	
