fdfd926265a21e0cdd538ccf50424078f75dfda8
Nick Bellamy <nick.bellamy@gmail.com>
2016-01-18 13:52:33 +0000

    Add Pause/Play button

---
+++ Assets/Scripts/UIButtons.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIButtons : MonoBehaviour {
+	
+	private LevelManager levelManager;
+	private bool isPaused = false;
+	private Text text;
+	private GameObject messageCanvas;
+	private GameObject pauseCanvas;
+	
+	void Awake() {
+		pauseCanvas = GameObject.Find ("Pause Canvas");
+	}
+	
+	void Start() {
+		text = gameObject.GetComponent<Text>();
+		messageCanvas = GameObject.Find("Level Messages");
+		pauseCanvas.SetActive(false);
+	}
+	
+	
+	void OnMouseDown() {
+		if (isPaused) {
+			Time.timeScale = 1.0f;
+			text.text = "Pause >";
+			messageCanvas.transform.position -= new Vector3 (1000.0f, 1000.0f, 0.0f);
+		} else {
+			Time.timeScale = 0.0f;
+			text.text = "Play >";
+			messageCanvas.transform.position += new Vector3 (1000.0f, 1000.0f, 0.0f);
+		}
+		AudioListener.pause = !AudioListener.pause;
+		isPaused = !isPaused;
+		pauseCanvas.SetActive(isPaused);
+	}
+}
